1a35c7ae92835a45f7b15d12d01bb5d5359f68ed
hugmouse <mysh@tuta.io>
2021-05-17 15:08:15 +0800

    Possible fix i guess

--- gemax/client_tls.go
+++ gemax/client_tls.go
@@ -2,14 +2,22 @@ package gemax
 
 import (
 	"crypto/tls"
+	"errors"
 )
+
+// ErrInvalidServerName means that the server certificate doesn't match the server domain.
+var ErrInvalidServerName = errors.New("server domain and server TLS domain name don't match")
 
 func tlsVerifyDomain(cs *tls.ConnectionState, domain string) (err error) {
 	for _, cert := range cs.PeerCertificates {
+		// Workaround for "x509: certificate relies on legacy Common Name field, use SANs"
+		if cert.Subject.CommonName == domain {
+			return nil
+		}
 		err = cert.VerifyHostname(domain)
-		if err != nil {
-			return err
+		if err == nil {
+			return nil
 		}
 	}
-	return nil
+	return ErrInvalidServerName
 }
